543e19bcb7d215ccd9b2368746f799d7302af4ca
Mohamed Adel <madel7586@gmail.com>
2025-07-27 18:11:54 +0300

    add additional route constants for food details, cart, and profile

--- lib/core/router/routes.dart
+++ lib/core/router/routes.dart
@@ -3,4 +3,7 @@ class Routes{
     static const String login = 'login';
     static const String register = 'register';
     static const String home = 'home';
+    static const String foodDetails = 'foodDetails';
+    static const String cart = 'cart';
+    static const String profile = 'profile';
 }
